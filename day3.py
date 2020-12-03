9268badf62e8a376e29fa4706849c613e5a3285e
Cameron Poole <Cameron.j.poole@gmail.com>
2020-12-03 13:39:47 +0000

    Day 3 complete

---
+++ day3.py
@@ -0,0 +1,42 @@
+import operator
+from functools import reduce
+from pathlib import Path
+
+test_input = """..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"""
+
+
+def travese_slope(input_map: list, right=3, down=1):
+    tobagan = 0
+    hit_tree = 0
+    for idx, line in enumerate(input_map):
+        # start at one
+        if idx == 0 or idx % down:
+            continue
+        line = line.rstrip("\n")
+        tobagan += right
+        tobagan_pos = tobagan % len(line)
+        if line[tobagan_pos] == "#":
+            hit_tree += 1
+    return hit_tree
+
+
+if __name__ == '__main__':
+    test_count = travese_slope(test_input.split("\n"))
+    assert test_count == 7, f"test count was {test_count}"
+    with open(Path("inputs").joinpath("day3.txt"), "r") as input_data:
+        snow_map = input_data.readlines()
+        print(f"Part 1 = {travese_slope(snow_map)}")
+        paths = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+        print(f"Part 2 = {reduce(operator.mul,[ travese_slope(snow_map, *_) for _ in paths ])}")
+
+
